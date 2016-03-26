c9602de310db40fd3b6e6bc5d9c7026af7a351b0
Gavin Cabbage <gavin.cabbage@capitalone.com>
2016-03-26 16:50:26 -0400

    first attempt at travis gh-pages deploy

---
+++ bin/deploy.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -e
+
+if [ "${TRAVIS_REPO_SLUG}" != "gavincabbage/bos-mission-planner" ||
+     "${TRAVIS_PULL_REQUEST}" != "false" ||
+     "${TRAVIS_BRANCH}" != "master" ]
+then
+    exit 1
+fi
+
+cd $HOME
+
+pwd
+ls
+
+exit 0
+
+# rm -rf dist
+# mkdir dist
+#
+# # complile step
+# # will probably need to do bower install here
+# cp -R static/* dist/
+#
+# cd dist
+# git init
+#
+# git config user.name "Travis CI"
+# git config user.email "gavincabbage@gmail.com"
+#
+# git add .
+# git commit -m "deploying bos-mission-planner to gh-pages"
+#
+# git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
