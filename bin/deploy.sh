b58ed85b092276c3d5890abf83ef2888cd6e687b
Gavin Cabbage <gavin.cabbage@capitalone.com>
2016-03-26 17:50:30 -0400

    travis real run

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -15,23 +15,23 @@ cd $HOME
 echo 123abczyx
 pwd
 ls
-
-exit 0
+#
+# exit 0
 
 # rm -rf dist
 # mkdir dist
-#
-# # complile step
-# # will probably need to do bower install here
-# cp -R static/* dist/
-#
-# cd dist
-# git init
-#
-# git config user.name "Travis CI"
-# git config user.email "gavincabbage@gmail.com"
-#
-# git add .
-# git commit -m "deploying bos-mission-planner to gh-pages"
-#
-# git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+
+# complile step
+# will probably need to do bower install here
+cp -R bos-mission-planner/static/ dist/
+
+cd dist
+git init
+
+git config user.name "Travis CI"
+git config user.email "gavincabbage@gmail.com"
+
+git add .
+git commit -m "deploying bos-mission-planner to gh-pages"
+
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
