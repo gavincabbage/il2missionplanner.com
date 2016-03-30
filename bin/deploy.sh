7f76846cfe299c7c6b18316e306bfe7eb82d404f
Gavin Cabbage <gavin.cabbage@capitalone.com>
2016-03-30 14:26:41 -0400

    updated deploy script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,43 +1,83 @@
 #!/bin/bash
 
-set -e
+REPO="gavincabbage/bos-mission-planner"
+BETA_BRANCH="develop"
+PROD_BRANCH="master"
+SRC="static/"
+DIST="dist/"
+GIT_NAM="Gavin Cabbage"
+GIT_EMAIL="gavincabbage@gmail.com"
+GIT_COMMIT_MSG="deploying to gh-pages"
 
-if [ "${TRAVIS_REPO_SLUG}" != "gavincabbage/bos-mission-planner" ] || \
-   [ "${TRAVIS_PULL_REQUEST}" != "false" ] || \
-   [ "${TRAVIS_BRANCH}" != "develop" ]
-then
-    echo "Not develop, aborting beta deploy happily"
+LOG="echo ["`basename ${0}`"] "
 
-    if [ "${TRAVIS_BRANCH}" == "master" ]
+function preamble() {
+    set -o errexit
+    set -o errtrace
+    set -o nounset
+    set -o xtrace
+    set -o pipefail
+}
+
+function abort { # usage: abort <code> <message>
+    ${LOG}${2}": exit "${1}
+    exit ${1}
+}
+
+function build {
+    $LOG"Starting build"
+    bower install
+    $LOG"Build successful"
+}
+
+function deploy_prod {
+    $LOG"Starting prod deployment"
+    cp -R ${SRC} ${DIST}
+    $LOG"Ready for automatic deployment to S3"
+}
+
+function deploy_beta {
+    $LOG"Starting beta deployment"
+    cd ..
+    cp -R bos-mission-planner/${SRC} ${DIST}
+    cd ${DIST}
+    $LOG"Initializing git repo"
+    git init
+    git config user.name ${GIT_NAME}
+    git config user.email ${GIT_EMAIL}
+    git add .
+    git commit -m ${GIT_COMMIT_MSG}
+    $LOG"Pushing to gh-pages"
+    git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+}
+
+function main() {
+
+    # Do not deploy any PRs or code from other repos
+    if [ "${TRAVIS_REPO_SLUG}" != "${REPO}" ] || \
+       [ "${TRAVIS_PULL_REQUEST}" != "false" ] || \
+       ([ "${TRAVIS_BRANCH}" != "${PROD_BRANCH}" ] && \
+        [ "${TRAVIS_BRANCH}" != "${BETA_BRANCH}" ])
     then
-        echo "Moving static to dist for master deploy"
-        cp -R static/ dist/
+        abort "0" "No deploy necessary - exiting happily"
     fi
 
-    exit 0
-fi
+    build()
 
-echo "New code merged to develop, deploying to gh-pages"
-pwd
-bower install
-cd ..
-pwd
-cp -R bos-mission-planner/static/ dist/
-echo "Copied successfully"
+    # Deploy depending on branch
+    if [ "${TRAVIS_BRANCH}" == "${PROD_BRANCH}" ]
+    then
+        ${LOG}"Deploying branch:${PROD_BRANCH} to production"
+        deploy_prod()
 
-cd dist
-git init
-git config user.name "Gavin Cabbage"
-git config user.email "gavincabbage@gmail.com"
-git add .
-git commit -m "deploying bos-mission-planner to gh-pages"
-echo "Committed successfully"
+    elif [ "${TRAVIS_BRANCH}" == "${BETA_BRANCH}" ]
+    then
+        ${LOG}"Deploying branch:${BETA_BRANCH} to beta"
+        deploy_beta()
+    fi
 
-echo "Branch info"
-git branch
+    abort "0" "Finished deployment successfully!"
+}
 
-echo "Finished configuring repo, pushing..."
-
-git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
-
-echo "Deployed to gh-pages successfully!"
+preamble()
+main()
