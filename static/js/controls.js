510e82504ac8ca8293fbd9a5347b97327f2919c1
Gavin Cabbage <gavin.cabbage@capitalone.com>
2016-03-29 20:53:38 -0400

    controls made obsolete, removed skeleton dep, pulled content from dead branch

--- static/js/controls.js
+++ static/js/controls.js
@@ -1,14 +1,14 @@
-(function() {
-
-    // Monkey patch existing Leaflet.draw classes
-
-    L.Draw.Polyline = L.Draw.Polyline.extend({
-        options: {
-            shapeOptions: {
-                color: '#ff0000',
-                weight: 2
-            }
-        }
-    });
-
-})();
+// (function() {
+//
+//     // Monkey patch existing Leaflet.draw classes
+//
+//     // L.Draw.Polyline = L.Draw.Polyline.extend({
+//     //     options: {
+//     //         shapeOptions: {
+//     //             color: '#ff0000',
+//     //             weight: 2
+//     //         }
+//     //     }
+//     // });
+//
+// })();
