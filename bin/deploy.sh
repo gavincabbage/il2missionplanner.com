7c7ac6bfe30315391876a7ffbbe0dd76cb8c299a
Gavin Cabbage <gavin.cabbage@capitalone.com>
2016-03-30 17:58:02 -0400

    quieting git commit in deploy script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -48,7 +48,7 @@ function deploy_beta {
     git config user.name "${GIT_NAME}"
     git config user.email "${GIT_EMAIL}"
     git add .
-    git commit -m "${GIT_COMMIT_MSG}"
+    git commit --quiet -m "${GIT_COMMIT_MSG}"
     log "Pushing to gh-pages"
     git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
 }
