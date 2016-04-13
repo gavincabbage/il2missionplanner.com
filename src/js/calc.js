5f2c3ece3fc83191c7af6b84554b6870dc2d3f09
Gavin Cabbage <gavin.cabbage@capitalone.com>
2016-04-13 10:26:15 -0400

    removed img folder

---
+++ src/js/calc.js
@@ -0,0 +1,50 @@
+module.exports = (function() {
+
+    const
+        SECONDS_IN_HOUR = 3600
+    ;
+
+    return {
+
+        distance: function(a, b) {
+            var dLng = b.lng - a.lng;
+            var dLat = b.lat - a.lat;
+            return Math.sqrt(dLng * dLng + dLat * dLat);
+        },
+
+        geometricDegreesToGeographic: function(degrees) {
+            if (degrees < 0) {
+                degrees += 360;
+            }
+            return (450 - degrees) % 360;
+        },
+
+        heading: function(a, b) {
+            var radians = Math.atan2(b.lat - a.lat, b.lng - a.lng);
+            var degrees = radians * 180 / Math.PI;
+            degrees = this.geometricDegreesToGeographic(degrees);
+            return degrees;
+        },
+
+        midpoint: function(a, b) {
+            var lat = (a.lat + b.lat) / 2;
+            var lng = (a.lng + b.lng) / 2;
+            return L.latLng(lat, lng);
+        },
+
+        pad: function(num, size) {
+            var s = Math.floor(num).toFixed(0);
+            while (s.length < size) {
+                s = "0" + s;
+            }
+            return s;
+        },
+
+        time: function(speed, distance) {
+            var kmPerSecond = speed / SECONDS_IN_HOUR;
+            return distance / kmPerSecond;
+        }
+
+    };
+
+})();
---
+++ src/js/util.js
@@ -0,0 +1,15 @@
+module.exports = (function() {
+
+    var calc = require('./calc.js');
+
+    return {
+
+        formatTime: function(timeInSeconds) {
+            var minutes = timeInSeconds / 60;
+            var seconds = timeInSeconds % 60;
+            return Math.floor(minutes).toFixed(0) + ':' + calc.pad(seconds, 2);
+        }
+
+    };
+
+})();
