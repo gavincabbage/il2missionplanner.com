d0e514cffd381cf25a54a779b725a85b3a989c31
Gavin Cabbage <gavincabbage@gmail.com>
2017-09-23 17:22:48 -0400

    fixed download; added map specific zoom max and min; enabled edit on mobile to test

--- app/js/util.js
+++ app/js/util.js
@@ -89,7 +89,25 @@ module.exports = (function() {
                 var reg = new RegExp('(\\s|^)' + className + '(\\s|$)');
                 el.className=el.className.replace(reg, ' ');
             }
-        }
+        },
         // End class functions
+
+        // Download function taken from here https://stackoverflow.com/questions/2897619/using-html5-javascript-to-generate-and-save-a-file
+        download: function(filename, text) {
+            var pom = document.createElement('a');
+            pom.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+            pom.setAttribute('download', filename);
+
+            if (document.createEvent) {
+                var event = document.createEvent('MouseEvents');
+                event.initEvent('click', true, true);
+                pom.dispatchEvent(event);
+            }
+            else {
+                pom.click();
+            }
+        }
+        // End download function
+
     };
 })();
